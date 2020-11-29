2a840a7ddaa3241eb3da6a1cce4e01384620a91b
Ian Flores Siaca <ian-flores-siaca@Ians-MacBook-Pro.local>
2020-11-29 14:17:29 -0400

    Adding `get_all_articles_info`

---
+++ R/get_all_articles_info.R
@@ -0,0 +1,11 @@
+#' @export
+
+get_all_articles_info <- function(urls){
+  articles_info <- list()
+
+  for (idx in 1:length(urls)){
+    articles_info[[idx]] <- as_tibble(get_article(urls[idx]))
+  }
+
+  return(articles_info)
+}
