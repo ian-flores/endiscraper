d75d2a22af93eb7644392b7d9434ecb7cc8ad5d0
Ian Flores Siaca <ian-flores-siaca@Ians-MacBook-Pro.local>
2020-11-29 14:29:52 -0400

    Adding `get_all_articles_info`

--- R/get_all_articles_info.R
+++ R/get_all_articles_info.R
@@ -4,7 +4,7 @@ get_all_articles_info <- function(urls){
   articles_info <- list()
 
   for (idx in 1:length(urls)){
-    articles_info[[idx]] <- tibble::as_tibble(get_article(urls[idx])) %>% mutate(urls = urls[idx])
+    articles_info[[idx]] <- tibble::as_tibble(get_article(urls[idx])) %>% dplyr::mutate(urls = urls[idx])
   }
 
   return(articles_info)
